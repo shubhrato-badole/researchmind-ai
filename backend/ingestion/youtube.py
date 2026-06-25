7cfbf7ad4e700bef46e1866541965610b7f5f46f
Shubhrato Badole <bshubhrato@gmail.com>
2026-06-25 12:46:13 +0530

    feat(rag): add YouTube transcript ingestion

---
+++ backend/ingestion/youtube.py
@@ -0,0 +1,9 @@
+from langchain_community.document_loaders import YoutubeLoader
+from utils import process_and_store
+
+
+def ingest_youtube(url:str , user_id:int):
+    loader =  YoutubeLoader.from_youtube_url(url, add_video_info=True)
+    docs = loader.load()
+
+    return process_and_store(docs, url, "youtube", url, user_id)
