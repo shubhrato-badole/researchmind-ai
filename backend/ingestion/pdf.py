aa2d6b500577fd0c1c48e559c893e5b58ae7ada7
Shubhrato Badole <bshubhrato@gmail.com>
2026-06-25 12:37:11 +0530

    feat(rag): add PDF document ingestion and processing

---
+++ backend/ingestion/pdf.py
@@ -0,0 +1,16 @@
+from langchain_community.document_loaders import PyPDFLoader
+from ingestion.utils import process_and_store
+import tempfile # this isused ot create temprorpy file in the system that get deletd after creacting an document of that 
+import os
+
+
+def ingest_pdf(file_bytes: bytes, filename: str, user_id: int):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+        tmp.write(file_bytes)
+        tmp_path= tmp.name
+
+        loader= PyPDFLoader(tmp_path)
+        docs = loader.load()
+
+        os.unlink(tmp_path)
+        return process_and_store(docs, filename, "pdf", None, user_id)
