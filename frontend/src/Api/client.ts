b10c4ce82d486318720cdbd74f2e375a1f646781
Shubhrato Badole <bshubhrato@gmail.com>
2026-07-16 17:15:17 +0530

    Connect frontend to Render backend

--- frontend/src/Api/client.ts
+++ frontend/src/Api/client.ts
@@ -1,31 +1,27 @@
 import axios from 'axios'
 
-
 const client = axios.create({
-  baseURL: '/api',
-  withCredentials: true 
-
+  baseURL: import.meta.env.VITE_API_URL + '/api',
+  withCredentials: true
 })
 
-
-
 axios.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        if(error.response?.status === 401){
-            try{
-                await axios.post('api/auth/ref' , {} , {withCredentials :true})
-                return client(error.config)
-
-            }catch{
-                window.location.href ="/login"
-
-            }
-        }
-         return Promise.reject(error)
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/api/auth/ref`,
+          {},
+          { withCredentials: true }
+        )
+        return client(error.config)
+      } catch {
+        window.location.href = "/login"
+      }
     }
-
+    return Promise.reject(error)
+  }
 )
 
 export default client
-
